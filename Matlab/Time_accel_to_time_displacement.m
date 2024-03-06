c7004112e8c3debd3662c04cc4ecdd6614afd2bc
Hao <hao.xiang.yang2000@gmail.com>
2024-03-06 18:29:22 -0500

    Added matlab function to convert acceleration data to displacement data

--- Matlab/Time_accel_to_time_displacement.m
+++ Matlab/Time_accel_to_time_displacement.m
@@ -1,1 +1,21 @@
-reshape
+% Input name of file here
+name = "GM1_Acc(g).txt";
+
+% Open text file
+fileID = fopen('GM1_Acc(g).txt','r');
+formatSpec = '%f\t%f';
+sizeA = [2 Inf];
+
+% Find displacement form acceleration
+Accel = fscanf(fileID,formatSpec, sizeA);
+accel_data = Accel(2,:).';
+time_data = Accel(1,:).';
+dt = time_data(5)-time_data(4);
+disp_data = acc2disp(accel_data,dt);
+
+figure
+plot(time_data, accel_data)
+title("Time-Acceleration Data")
+figure
+plot(time_data, disp_data)
+title("Time-Displacement Data")
