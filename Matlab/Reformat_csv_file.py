5a77783bcefb08a5798c51b555c67853ad4f32f7
Hao <hao.xiang.yang2000@gmail.com>
2024-03-07 18:08:41 -0500

    Upload data directly to arduino

--- Matlab/Reformat_csv_file.py
+++ Matlab/Reformat_csv_file.py
@@ -3,13 +3,12 @@ import csv
 new_text = []
 with open('Data\\2024\\GM1_Dis(cm).txt', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    counter = 0
     for row in spamreader:
-        new_row = '{' + ', '.join(row) + '},'
-        new_text.append(new_row)
+        if counter ==51:
+            print(row)
 
-with open('Data\\2024\\GM1_Dis(cm)_convert.txt', 'w', newline='') as csvfile:
-    for row in new_text:
-        csvfile.write("%s\n" % row)
+        counter+=1
 
 
 print("done")
